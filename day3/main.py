8d3406e41f6eaa0bef50616f7bdf10122a06713f
Rohan pandey <rohanrock4019@gmail.com>
2026-09-13 15:31:49 +0530

    add main FastAPI application with CRUD operations

---
+++ day3/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"message":"hello rohan all good."}
+
+@app.post("/items/")
+def create_item(name: str,price: float):
+    return{"name":name , "price":price}
+
+@app.put("/items/{id}")
+def update_item(id: int,name: str,price: float):
+    return{"id":id,"name":name,"price":price}
+
+@app.delete("/items/{id}")
+def delete_item(id:int):
+    return{"id":f"item {id} deleted succesfully"}
