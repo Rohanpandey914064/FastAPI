f8a63e1aaff1d9bba3b1569338e4636c4bc9eaac
Rohan pandey <rohanrock4019@gmail.com>
2026-09-13 16:17:48 +0530

    add main FastAPI application with user details endpoint

---
+++ day4/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"message":"hello rohan all good."}
+
+# @app.get("/users/{user_id}")
+# def read_users(user_id:int):
+#     return{"Rohan user id is",user_id}
+
+# @app.get("/users/{name}")
+# def read_users(name:str):
+#     return{"Rohan user id is",name}
+
+# @app.get("/users/")
+# def read_users(user_id:int,name:str):
+#     return{"id":user_id,"name":name}
+
+@app.get("/users/{user_id}/details")
+def read_users(user_id:int,include_email:bool=False):
+    if include_email:
+        return{"id":user_id, "include_email":"email available bro"}
+    else:
+        return{"id":user_id,"include_email":"email not available bro"}
+    
