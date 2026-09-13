ce679720ae234b382897d9d87a784bb979bc8133
Rohan pandey <rohanrock4019@gmail.com>
2026-09-14 01:41:02 +0530

    refactor User model and enhance validation in main.py

--- day4/main.py
+++ day4/main.py
@@ -1,14 +1,14 @@
 # Request and Response -- 
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 app = FastAPI()
 
-class User(BaseModel):
-    name:str
-    age:int
-    number:int
+# class User(BaseModel):
+#     name:str
+#     age:int
+#     number:int
 
 @app.get("/")
 def read_root():
@@ -22,8 +22,23 @@ def read_root():
 # responce section 
 # @app.get("/users/{user_id}",response_model=User)
 # async def getUser(user_id:int):
-#     return{"name":"Rohan pandey","age":21}
+#     return{"name":"Rohan pandey","age":21,"number":234}
 
 
 
 # Example
+
+class User(BaseModel):
+    name:str
+    age:int = Field(..., gt=0, le=120)
+
+    @field_validator("name")
+    def name_must_not_be_empty(cls, v):
+        if not v:
+            raise ValueError("Name must not be empty")
+        return v
+
+@app.post("/users/")
+async def create_user(user:User):
+    u={"name":user.name, "age":user.age}
+    return u
